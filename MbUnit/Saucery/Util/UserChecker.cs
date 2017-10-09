4e49130523743d88aa044bafdbdc222ee7fe76f1
Andrew Gray <andrew.paul.gray@gmail.com>
2017-10-09 17:51:22 +1100

    remove MBUnit implementation as too old

--- MbUnit/Saucery/Util/UserChecker.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Saucery.Util {
-    internal static class UserChecker {
-        internal static bool ItIsMe() {
-            return SauceryConstants.SAUCE_USER_NAME.ToLower().Equals(SauceryConstants.MY_USERNAME);
-        }
-    }
-}
