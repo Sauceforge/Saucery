c9ad09281a348556443646759e13d9f61c6f7d3f
Andrew Gray <andrew.gray@coles.com.au>
2022-03-18 20:41:07 +1100

    call function once only

--- Saucery/Dojo/PlatformConfigurator.cs
+++ Saucery/Dojo/PlatformConfigurator.cs
@@ -12,49 +12,9 @@ namespace Saucery.Dojo
 
         public PlatformConfigurator(List<SupportedPlatform> platforms)
         {
-            //To Keep
-            //windows_platforms           (898)
-            //mac1010_webdriver_platforms (70)
-            //mac1011_webdriver_platforms (151)
-            //mac1012_webdriver_platforms (190)
-            //mac1013_webdriver_platforms (191)
-            //mac1014_webdriver_platforms (189)
-            //mac1015_webdriver_platforms (149)
-            //mac11_webdriver_platforms   (93)
-            //mac12_webdriver_platforms   (93)
-            //ios_appium_platforms        (460)   //IOS versions is short_version. All have recommended backend version.
-            //android_appium_platforms    (159)
-            //TOTAL                       2494
-
-            //var windows_platforms = platforms.FindAll(p => p.os.Contains("Windows") && p.automation_backend.Equals("webdriver"));
-
-            //var mac1010_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.10") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac1011_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.11") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac1012_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.12") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac1013_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.13") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac1014_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.14") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac1015_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 10.15") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac11_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 11") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-            //var mac12_webdriver_platforms = platforms.FindAll(p => p.os.Equals("Mac 12") && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
-
-            //var ios_appium_platforms = platforms.FindAll(p => (p.api_name.Equals("iphone") || p.api_name.Equals("ipad")) && p.automation_backend.Equals("appium"));
-            //var ios_appium_platforms_byversion = ios_appium_platforms.GroupBy(g => g.recommended_backend_version);
-
-            //var android_webdriver_platforms = platforms.FindAll(p => p.api_name.Equals("android") && p.automation_backend.Equals("webdriver"));
-            //var android_webdriver_platforms_byversion = android_webdriver_platforms.GroupBy(g => g.recommended_backend_version);
-            //var android_appium_platforms = platforms.FindAll(p => p.api_name.Equals("android") && p.automation_backend.Equals("appium"));
-            //var android_appium_platforms_byversion = android_appium_platforms.GroupBy(g => g.recommended_backend_version);
-
             var filteredPlatforms = new List<SupportedPlatform>();
             filteredPlatforms.AddRange(FindWindowsPlatforms(platforms));          //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.10")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.11")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.12")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.13")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.14")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 10.15")); //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 11"));    //Not filtered for Min and Max Versions yet
-            filteredPlatforms.AddRange(FindMacPlatforms(platforms, "Mac 12"));    //Not filtered for Min and Max Versions yet
+            filteredPlatforms.AddRange(FindMacPlatforms(platforms, new List<string> { "Mac 10.10", "Mac 10.11", "Mac 10.12", "Mac 10.13", "Mac 10.14", "Mac 10.15", "Mac 11", "Mac 12" }));
             filteredPlatforms.AddRange(FindMobilePlatforms(platforms, new List<string> { "iphone", "ipad" }));
             filteredPlatforms.AddRange(FindMobilePlatforms(platforms, new List<string> { "android" }));
 
@@ -70,9 +30,9 @@ namespace Saucery.Dojo
 
         private static List<SupportedPlatform> FindWindowsPlatforms(List<SupportedPlatform> platforms) => platforms.FindAll(p => p.os.Contains("Windows") && p.automation_backend.Equals("webdriver"));
 
-        private static List<SupportedPlatform> FindMacPlatforms(List<SupportedPlatform> platforms, string os) => platforms.FindAll(p => p.os.Equals(os) && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
+        private static List<SupportedPlatform> FindMacPlatforms(List<SupportedPlatform> platforms, List<string> oses) => platforms.FindAll(p => oses.Any(o => o.Equals(p.os)) && p.automation_backend.Equals("webdriver") && !p.api_name.Equals("ipad") && !p.api_name.Equals("iphone"));
 
-        private List<SupportedPlatform> FindMobilePlatforms(List<SupportedPlatform> platforms, List<string> apis) => platforms.FindAll(p => apis.Any(a => a.Equals(p.api_name)) && p.automation_backend.Equals("appium"));
+        private static List<SupportedPlatform> FindMobilePlatforms(List<SupportedPlatform> platforms, List<string> apis) => platforms.FindAll(p => apis.Any(a => a.Equals(p.api_name)) && p.automation_backend.Equals("appium"));
 
         public void AddLatestBrowserVersion(string version)
         {
@@ -100,7 +60,6 @@ namespace Saucery.Dojo
         public BrowserVersion Validate(SaucePlatform requested)
         {
             BrowserVersion browserVersion = null;
-            //TODO: Needs work
             switch (requested.PlatformType)
             {
                 case PlatformType.Chrome:
