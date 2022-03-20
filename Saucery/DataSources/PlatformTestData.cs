06a18a9f8646822dd4b86dd10a1a4a9f09c2b8b3
Andrew Gray <andrew.gray@coles.com.au>
2022-03-21 00:22:21 +1100

    match selenium4 branch

--- Saucery/DataSources/PlatformTestData.cs
+++ Saucery/DataSources/PlatformTestData.cs
@@ -5,6 +5,7 @@ using Saucery.OnDemand;
 using Saucery.Util;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Saucery.DataSources
 {
@@ -16,22 +17,23 @@ namespace Saucery.DataSources
         internal static Compositor Compositor { get; set; }
         //internal static CompositorBuilder Builder { get; set; }
 
-        //internal string Os { get; set; }
-        //internal string Platform { get; set; }
-        //internal string BrowserName { get; set; }
-        //internal string BrowserVersion { get; set; }
-        //internal string LongName { get; set; }
-        //internal string LongVersion { get; set; }
-        //internal string Url { get; set; }
-        //internal string Device { get; set; }
-        //internal string DeviceType { get; set; }
-        //internal string DeviceOrientation { get; set; }
+        internal string Os { get; set; }
+        internal string Platform { get; set; }
+        internal string BrowserName { get; set; }
+        internal string BrowserVersion { get; set; }
+        internal string LongName { get; set; }
+        internal string LongVersion { get; set; }
+        internal string Url { get; set; }
+        internal string Device { get; set; }
+        internal string DeviceType { get; set; }
+        internal string DeviceOrientation { get; set; }
         #endregion
 
         #region Methods
-        //internal double ParseBrowserVersion() {
-        //    return double.Parse(BrowserVersion);
-        //}
+        internal double ParseBrowserVersion()
+        {
+            return double.Parse(BrowserVersion);
+        }
 
         #endregion
 
@@ -41,11 +43,24 @@ namespace Saucery.DataSources
             Compositor = CompositorBuilder.Build();
             Compositor.Compose();
             Platforms = JsonConvert.DeserializeObject<List<SaucePlatform>>(Enviro.SauceOnDemandBrowsers);
+            Platforms.ClassifyAll();
             BrowserVersions = new PlatformConfigurator().Filter(Platforms);
         }
 
         public IEnumerator GetEnumerator() {
-            return BrowserVersions?.GetEnumerator();
+            //return BrowserVersions?.GetEnumerator(); //TODO: New way
+
+            return Platforms?.Select(platform => new SaucePlatform(platform.Os,
+                                                                   platform.Browser,
+                                                                   platform.BrowserVersion,
+                                                                   platform.Platform,
+                                                                   platform.LongName,
+                                                                   platform.LongVersion,
+                                                                   platform.Url,
+                                                                   platform.Device,
+                                                                   platform.AppiumVersion,
+                                                                   platform.DeviceOrientation).Classify()).GetEnumerator();
+
         }
     }
 }
