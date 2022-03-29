96f3d1daaabb2310c841a309fcb8528fe85f0cef
Andrew Gray <andrew.paul.gray@gmail.com>
2022-03-29 13:02:37 +1100

    clean up

--- Saucery/Options/ConcreteProducts/AppiumIOSOptions.cs
+++ Saucery/Options/ConcreteProducts/AppiumIOSOptions.cs
@@ -9,39 +9,22 @@ namespace Saucery.Options.ConcreteProducts
     internal class AppiumIOSOptions : BaseOptions {
         public AppiumIOSOptions(BrowserVersion browserVersion, string testName) : base(testName) {
             Console.WriteLine(SauceryConstants.SETTING_UP, testName, SauceryConstants.IOS_ON_APPIUM);
-            AddSauceLabsOptions(Enviro.SauceNativeApp);
-
             DebugMessages.PrintiOSOptionValues(browserVersion);
-
             Console.WriteLine("Creating iOS Options");
 
-            //var options = new AppiumOptions()
-            //{
-            //    PlatformName = browserVersion.PlatformNameForOption,
-            //    BrowserName = SauceryConstants.SAFARI_BROWSER,
-            //    DeviceName = browserVersion.DeviceName,
-            //    PlatformVersion = browserVersion.Name
-            //};
+            AddSauceLabsOptions(Enviro.SauceNativeApp);
 
-            //SauceOptions.Add(SauceryConstants.SAUCE_APPIUM_VERSION_CAPABILITY, browserVersion.RecommendedAppiumVersion);
-            //options.AddAdditionalAppiumOption(SauceryConstants.SAUCE_OPTIONS_CAPABILITY, SauceOptions);
-            //Opts = options;
+            var options = new AppiumOptions
+            {
+                PlatformName = browserVersion.PlatformNameForOption,
+                BrowserName = SauceryConstants.SAFARI_BROWSER,
+                DeviceName = browserVersion.DeviceName,
+                PlatformVersion = browserVersion.Name
+            };
 
-            var options = new AppiumOptions();
-            options.PlatformName = browserVersion.PlatformNameForOption;
             options.AddAdditionalAppiumOption("platformName", browserVersion.PlatformNameForOption);
-            
-            options.BrowserName = SauceryConstants.SAFARI_BROWSER;
             options.AddAdditionalAppiumOption("browserName", SauceryConstants.SAFARI_BROWSER);
-            //options.AddAdditionalOption("appium:deviceName", browserVersion.DeviceName);
-            //options.AddAdditionalOption("appium:platformVersion", browserVersion.Name);
             
-            options.DeviceName = browserVersion.DeviceName;
-            //options.AddAdditionalAppiumOption("deviceName", browserVersion.DeviceName);
-            options.PlatformVersion = browserVersion.Name;
-
-            //var sauceOptions = new Dictionary<string, object>();
-            //sauceOptions.Add("appiumVersion", "1.22.2");
             SauceOptions.Add(SauceryConstants.SAUCE_APPIUM_VERSION_CAPABILITY, browserVersion.RecommendedAppiumVersion);
             options.AddAdditionalAppiumOption(SauceryConstants.SAUCE_OPTIONS_CAPABILITY, SauceOptions);
             Opts = options;
