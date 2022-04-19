4b3e48fc0b4d9561679b80a25e517fee430e4f78
Andrew Gray <andrew.paul.gray@gmail.com>
2022-04-19 11:37:55 +1000

    remove unused code

--- Saucery/Util/DebugMessages.cs
+++ Saucery/Util/DebugMessages.cs
@@ -5,19 +5,6 @@ using System;
 namespace Saucery.Util
 {
     public class DebugMessages {
-        //public static void PrintPlatformDetails(PlatformTestData platform) {
-        //    if (UserChecker.ItIsMe())
-        //    {
-        //        Console.WriteLine("DEBUG MESSAGE START");
-        //        Console.WriteLine(@"SELENIUM_OS=" + platform.Os);
-        //        Console.WriteLine(@"SELENIUM_BROWSER=" + platform.BrowserName);
-        //        Console.WriteLine(@"SELENIUM_VERSION=" + platform.BrowserVersion);
-        //        Console.WriteLine(@"SELENIUM_LONG_VERSION=" + platform.LongVersion);
-        //        Console.WriteLine(@"SELENIUM_DEVICE_TYPE=" + platform.DeviceType);
-        //        Console.WriteLine(@"SELENIUM_DEVICE=" + platform.Device);
-        //        Console.WriteLine("DEBUG MESSAGE END");
-        //    }
-        //}
         internal static void PrintHaveDesktopPlatform()
         {
             if (UserChecker.ItIsMe())
@@ -72,10 +59,7 @@ namespace Saucery.Util
                               SauceryConstants.SAUCE_PLATFORM_VERSION_CAPABILITY, browserVersion.Name,
                               SauceryConstants.SAUCE_PLATFORM_NAME_CAPABILITY, SauceryConstants.PLATFORM_IOS,
                               SauceryConstants.SAUCE_BROWSER_NAME_CAPABILITY, SauceryConstants.SAFARI_BROWSER,
-                              //SauceOpsConstants.SAUCE_DEVICE_CAPABILITY, platform.Device
-                              //SauceOpsConstants.SAUCE_DEVICE_CAPABILITY, platform.IsAnIPhone() ? SauceOpsConstants.IPHONE_DEVICE : SauceOpsConstants.IPAD_DEVICE
-                              SauceryConstants.SAUCE_DEVICE_CAPABILITY, browserVersion.IsAnIPhone() ? SauceryConstants.IPHONE_SIMULATOR : SauceryConstants.IPAD_SIMULATOR
-                              );
+                              SauceryConstants.SAUCE_DEVICE_CAPABILITY, browserVersion.IsAnIPhone() ? SauceryConstants.IPHONE_SIMULATOR : SauceryConstants.IPAD_SIMULATOR);
         }
 
         public static void PrintAndroidOptionValues(BrowserVersion browserVersion)
@@ -86,24 +70,6 @@ namespace Saucery.Util
                               SauceryConstants.SAUCE_BROWSER_NAME_CAPABILITY, SauceryConstants.CHROME_BROWSER,
                               SauceryConstants.SAUCE_PLATFORM_VERSION_CAPABILITY, browserVersion.Name,
                               SauceryConstants.SAUCE_PLATFORM_NAME_CAPABILITY, SauceryConstants.ANDROID);
-        }
-
-        internal static void UsingEnvCompositor()
-        {
-            if (UserChecker.ItIsMe())
-            {
-                Console.WriteLine("DEBUG MESSAGE: Building platforms from environment variable.");
-                Console.Out.Flush();
-            }
-        }
-
-        internal static void UsingBuiltInCompositor()
-        {
-            if (UserChecker.ItIsMe())
-            {
-                Console.WriteLine("DEBUG MESSAGE: Building platforms from Built In Compile Time set.");
-                Console.Out.Flush();
-            }
         }
     }
 }
