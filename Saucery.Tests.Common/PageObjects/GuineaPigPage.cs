a6f1fe63efb71c4f5ee776710a8a6a35bf4d374f
Andrew Gray <andrew.paul.gray@gmail.com>
2023-04-15 21:44:32 +1000

    create Saucery.Tests.Common project

--- Saucery.Tests.Common/PageObjects/GuineaPigPage.cs
+++ Saucery.Tests.Common/PageObjects/GuineaPigPage.cs
@@ -5,19 +5,16 @@ using Saucery.Core.PageObjects;
 using SeleniumExtras.PageObjects;
 using Shouldly;
 
-namespace Merlin.NUnit.PageObjects;
+namespace Saucery.Tests.Common.PageObjects;
 
-public class GuineaPigPage : PageObjectBase
-{
+public class GuineaPigPage : PageObjectBase {
     public GuineaPigPage(SauceryRemoteWebDriver driver, string urlRoot)
-        : base(urlRoot + "test/guinea-pig", "GuineaPig", "I am a page title - Sauce Labs")
-    {
+        : base(urlRoot + "test/guinea-pig", "GuineaPig", "I am a page title - Sauce Labs") {
         GetPage(driver);
         PageFactory.InitElements(driver, this);
     }
 
-    public GuineaPigPage ClickLink(SauceryRemoteWebDriver driver)
-    {
+    public GuineaPigPage ClickLink(SauceryRemoteWebDriver driver) {
         //Could also use a "Selectors" class here.
         var link = driver.FindElement(By.Id("i am a link"));
         link.Click();
@@ -30,8 +27,7 @@ public class GuineaPigPage : PageObjectBase
         //Could also use a "Selectors" class here.
         driver.FindElement(By.Id("useragent")).Text;
 
-    public GuineaPigPage TypeField(SauceryRemoteWebDriver driver, string fieldId, string data)
-    {
+    public GuineaPigPage TypeField(SauceryRemoteWebDriver driver, string fieldId, string data) {
         var element = driver.FindElement(By.Id(fieldId));
         element.Clear();
         element.SendKeys(data);
