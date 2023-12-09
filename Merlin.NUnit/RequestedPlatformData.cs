00561eb02c3bbd69454c6ac756159c45147ffb2e
Andrew Gray <andrew.paul.gray@gmail.com>
2023-12-09 12:12:41 +1100

    remove android

--- Merlin.NUnit/RequestedPlatformData.cs
+++ Merlin.NUnit/RequestedPlatformData.cs
@@ -12,7 +12,7 @@ public class RequestedPlatformData : SauceryTestData
         var platforms = new List<SaucePlatform>
         {
             //Mobile Platforms
-            new AndroidPlatform("Google Pixel 6 Pro GoogleAPI Emulator", "12.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
+            //new AndroidPlatform("Google Pixel 6 Pro GoogleAPI Emulator", "12.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
             new IOSPlatform("iPhone 13 Pro Max Simulator", "15.4", SauceryConstants.DEVICE_ORIENTATION_LANDSCAPE),
 
             //Desktop Platforms
--- Merlin.XUnit/RequestedPlatformData.cs
+++ Merlin.XUnit/RequestedPlatformData.cs
@@ -13,7 +13,7 @@ public class RequestedPlatformData : SauceryTestData
         List<SaucePlatform> platforms =
         [
             //Mobile Platforms
-            new AndroidPlatform("Google Pixel 8 Pro GoogleAPI Emulator", "14.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
+            //new AndroidPlatform("Google Pixel 8 Pro GoogleAPI Emulator", "14.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
             new IOSPlatform("iPhone 13 Pro Max Simulator", "15.4", SauceryConstants.DEVICE_ORIENTATION_LANDSCAPE),
 
             //Desktop Platforms
