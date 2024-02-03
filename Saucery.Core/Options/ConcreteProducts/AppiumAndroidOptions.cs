dae169823b7682bd9466598ab5e35465eeced585
Andrew Gray <andrew.paul.gray@gmail.com>
2024-02-03 14:57:12 +1100

    upgrade NuGet packages

--- Saucery.Core/Options/ConcreteProducts/AppiumAndroidOptions.cs
+++ Saucery.Core/Options/ConcreteProducts/AppiumAndroidOptions.cs
@@ -16,7 +16,7 @@ internal class AppiumAndroidOptions : BaseOptions {
 
         Console.WriteLine("Creating Appium Options");
 
-        var options = new AppiumOptions
+        AppiumOptions options = new() 
         {
             PlatformName = browserVersion.PlatformNameForOption,
             BrowserName = SauceryConstants.CHROME_BROWSER,
@@ -35,6 +35,29 @@ internal class AppiumAndroidOptions : BaseOptions {
         Opts = options;
     }
 }
+
+//Build: https://app.saucelabs.com/builds/vdc/129a1f32fc803fcbb0af8014fc54f4f7
+//https://app.saucelabs.com/tests/55649d023b9042829c4f4c3bfc3a92b4
+//https://app.saucelabs.com/tests/60c13baaa183414bbb66713b901a71d8
+
+//capabilities = new MutableCapabilities();
+//capabilities.setCapability("platformName", "Android");
+//capabilities.setCapability("browserName", "Chrome");
+//capabilities.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+//capabilities.setCapability("appium:newCommandTimeout", 90);
+//capabilities.setCapability("appium:platformVersion", "11.0");
+//capabilities.setCapability("automationName", "UiAutomator2");
+////capabilities.setCapability("name", name.getMethodName());
+
+//MutableCapabilities sauceOptions = new MutableCapabilities();
+//sauceOptions.setCapability("build", "supportTest");
+//sauceOptions.setCapability("name", "supportTest");
+//sauceOptions.setCapability("appiumVersion", "2.0.0");
+//sauceOptions.setCapability("sessionCreationTimeout", "10");
+//capabilities.setCapability("sauce:options", sauceOptions);
+
+//driver = new AndroidDriver<>(new URL("https://username:accesskey@ondemand.us-west-1.saucelabs.com:443/wd/hub"), capabilities);
+
 /*
 * Copyright Andrew Gray, SauceForge
 * Date: 5th February 2020
