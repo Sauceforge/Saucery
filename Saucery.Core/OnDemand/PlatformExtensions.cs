5a82cd594cd26d7b0fc54cca85c9ea48e0a3a4d3
Andrew Gray <andrew.paul.gray@gmail.com>
2024-06-23 14:07:58 +1000

    clean up messages

--- Saucery.Core/OnDemand/PlatformExtensions.cs
+++ Saucery.Core/OnDemand/PlatformExtensions.cs
@@ -7,24 +7,24 @@ namespace Saucery.Core.OnDemand;
 internal static class PlatformExtensions {
     public static bool IsAMobileDevice(this BrowserVersion browserVersion) => IsAnAndroidDevice(browserVersion) || IsAnAppleDevice(browserVersion);
 
-    public static bool IsAnAndroidDevice(this BrowserVersion browserVersion) => browserVersion.Os != null && browserVersion.Os.ToUpper().Contains(SauceryConstants.PLATFORM_LINUX.ToUpper()) && browserVersion.RecommendedAppiumVersion != null;
+    public static bool IsAnAndroidDevice(this BrowserVersion browserVersion) => browserVersion.Os != null && browserVersion.Os.Contains(SauceryConstants.PLATFORM_LINUX, StringComparison.CurrentCultureIgnoreCase) && browserVersion.RecommendedAppiumVersion != null;
 
     public static bool IsAnAppleDevice(this BrowserVersion browserVersion) => IsAnIPhone(browserVersion) || IsAnIPad(browserVersion);
 
-    public static bool IsAnIPhone(this BrowserVersion browserVersion) => browserVersion.DeviceName != null && browserVersion.DeviceName.ToLower().Contains(SauceryConstants.APPLE_IPHONE);
+    public static bool IsAnIPhone(this BrowserVersion browserVersion) => browserVersion.DeviceName != null && browserVersion.DeviceName.Contains(SauceryConstants.APPLE_IPHONE, StringComparison.CurrentCultureIgnoreCase);
 
-    public static bool IsAnIPad(this BrowserVersion browserVersion) => browserVersion.DeviceName != null && browserVersion.DeviceName.ToLower().Contains(SauceryConstants.APPLE_IPAD);
+    public static bool IsAnIPad(this BrowserVersion browserVersion) => browserVersion.DeviceName != null && browserVersion.DeviceName.Contains(SauceryConstants.APPLE_IPAD, StringComparison.CurrentCultureIgnoreCase);
 
     public static bool IsAnAppleDevice(this SaucePlatform platform) => IsAnIPhone(platform) || IsAnIPad(platform);
 
-    public static bool IsAnIPhone(this SaucePlatform platform) => platform.LongName != null && platform.LongName.ToLower().Contains(SauceryConstants.APPLE_IPHONE);
+    public static bool IsAnIPhone(this SaucePlatform platform) => platform.LongName != null && platform.LongName.Contains(SauceryConstants.APPLE_IPHONE, StringComparison.CurrentCultureIgnoreCase);
 
-    public static bool IsAnIPad(this SaucePlatform platform) => platform.LongName != null && platform.LongName.ToLower().Contains(SauceryConstants.APPLE_IPAD);
+    public static bool IsAnIPad(this SaucePlatform platform) => platform.LongName != null && platform.LongName.Contains(SauceryConstants.APPLE_IPAD, StringComparison.CurrentCultureIgnoreCase);
 
     public static bool IsAnAndroidDevice(this SaucePlatform platform) => platform.LongName != null &&
-               (platform.LongName.ToLower().Contains(SauceryConstants.GOOGLE_LOWER) ||
-                platform.LongName.ToLower().Contains(SauceryConstants.SAMSUNG_LOWER) ||
-                platform.LongName.ToLower().Contains(SauceryConstants.ANDROID_LOWER));
+               (platform.LongName.Contains(SauceryConstants.GOOGLE_LOWER, StringComparison.CurrentCultureIgnoreCase) ||
+                platform.LongName.Contains(SauceryConstants.SAMSUNG_LOWER, StringComparison.CurrentCultureIgnoreCase) ||
+                platform.LongName.Contains(SauceryConstants.ANDROID_LOWER, StringComparison.CurrentCultureIgnoreCase));
 }
 /*
 * Copyright Andrew Gray, SauceForge
