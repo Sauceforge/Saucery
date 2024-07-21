bc604dba49ab4cc7b5db2171f4cad64ac7c48bda
Andrew Gray <andrew.paul.gray@gmail.com>
2024-07-21 10:46:45 +1000

    webviewConnectTimeout appears to be ios only

--- Saucery.Core/Options/ConcreteProducts/RealDeviceIOSOptions.cs
+++ Saucery.Core/Options/ConcreteProducts/RealDeviceIOSOptions.cs
@@ -28,6 +28,7 @@ internal class RealDeviceIOSOptions : BaseOptions {
         //{
         //    SauceOptions.Add(SauceryConstants.SAUCE_DEVICE_ORIENTATION_CAPABILITY, browserVersion.DeviceOrientation);
         //}
+        options.AddAdditionalAppiumOption("webviewConnectTimeout", 50000);
         options.AddAdditionalAppiumOption(SauceryConstants.SAUCE_OPTIONS_CAPABILITY, SauceOptions);
         Opts = options;
     }
