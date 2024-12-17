b13c684d437547d3f23698e0f449544b12acbf40
Andrew Gray <andrew.paul.gray@gmail.com>
2024-12-17 15:37:18 +1100

    other TUnit test projects

---
+++ Merlin.TUnit.RealDevices/DataDrivenTests.cs
@@ -0,0 +1,36 @@
+﻿using Saucery.Core.Dojo;
+using Saucery.Tests.Common.PageObjects;
+using Saucery.TUnit;
+
+namespace Merlin.TUnit.RealDevices;
+
+public class DataDrivenTests : SauceryTBase
+{
+    [Test]
+    [MethodDataSource(nameof(AllCombinations), Arguments = [ new[] { 4, 5 } ])]
+    public async Task DataDrivenTest(BrowserVersion requestedPlatform, int data)
+    {
+        InitialiseDriver(requestedPlatform);
+
+        var guineaPigPage = new GuineaPigPage(SauceryDriver(), "https://saucelabs.com/");
+
+        guineaPigPage.TypeField(SauceryDriver(), "comments", data.ToString());
+    }
+
+    public static IEnumerable<(BrowserVersion, int)> AllCombinations(int[] data)
+    {
+        foreach (var browserVersion in RequestedPlatformData.AllPlatformsAsList())
+        {
+            foreach (var datum in data)
+            {
+                yield return (browserVersion, datum);
+            }
+        }
+    }
+}
+
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 7th December 2024
+* 
+*/
---
+++ Merlin.TUnit.RealDevices/MyParallelLimit.cs
@@ -0,0 +1,8 @@
+﻿using TUnit.Core.Interfaces;
+
+namespace Merlin.TUnit.RealDevices;
+
+public record MyParallelLimit : IParallelLimit
+{
+    public int Limit => 3;
+}
