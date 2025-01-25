5e40238310cd0c43aa72da56670912f86f108d7a
Andrew Gray <andrew.paul.gray@gmail.com>
2025-01-25 15:18:56 +1100

    Increase unit test coverage

--- Saucery.Core.Tests/PlatformTests.cs
+++ Saucery.Core.Tests/PlatformTests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using Saucery.Core.Dojo;
 using Saucery.Core.OnDemand;
 using Saucery.Core.OnDemand.Base;
-using Saucery.Core.Util;
+using Saucery.Core.Tests.DataProviders;
 using Shouldly;
 
 namespace Saucery.Core.Tests;
@@ -70,8 +70,8 @@ public class MerlinPlatformTests {
     [TestCaseSource(typeof(PlatformDataClass), nameof(PlatformDataClass.EmulatedIOSPlatforms))]
     [TestCaseSource(typeof(PlatformDataClass), nameof(PlatformDataClass.RealAndroidDevices))]
     [TestCaseSource(typeof(PlatformDataClass), nameof(PlatformDataClass.RealIOSDevices))]
-    public void TestNameTest(SaucePlatform platforms) {
-        var bvs = PlatformConfigurator!.FilterAll([platforms]);
+    public void TestNameTest(SaucePlatform platform) {
+        var bvs = PlatformConfigurator!.FilterAll([platform]);
 
         foreach(var bv in bvs) {
             bv.SetTestName(TestContext.CurrentContext.Test.Name);
@@ -101,59 +101,5 @@ public class MerlinPlatformTests {
         }
 
         _validCount.ShouldBeEquivalentTo(browserVersions.Count);
-    }
-
-    private static class PlatformDataClass {
-        public static List<SaucePlatform> DesktopPlatforms =>
-        [
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_11, SauceryConstants.BROWSER_CHROME, "123"),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_CHROME, "124", SauceryConstants.SCREENRES_2560_1600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_LINUX, SauceryConstants.BROWSER_CHROME, SauceryConstants.BROWSER_VERSION_LATEST),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_11, SauceryConstants.BROWSER_CHROME, "100->119", SauceryConstants.SCREENRES_2560_1600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_CHROME, SauceryConstants.BROWSER_VERSION_LATEST, SauceryConstants.SCREENRES_2560_1600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_81, SauceryConstants.BROWSER_CHROME, "100", SauceryConstants.SCREENRES_800_600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_8, SauceryConstants.BROWSER_FIREFOX, SauceryConstants.BROWSER_VERSION_LATEST),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_8, SauceryConstants.BROWSER_FIREFOX, SauceryConstants.BROWSER_VERSION_LATEST, SauceryConstants.SCREENRES_800_600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_MAC_11, SauceryConstants.BROWSER_CHROME, SauceryConstants.BROWSER_VERSION_LATEST),
-            new DesktopPlatform(SauceryConstants.PLATFORM_MAC_12, SauceryConstants.BROWSER_CHROME, SauceryConstants.BROWSER_VERSION_LATEST),
-            new DesktopPlatform(SauceryConstants.PLATFORM_MAC_13, SauceryConstants.BROWSER_CHROME, SauceryConstants.BROWSER_VERSION_LATEST),
-            new DesktopPlatform(SauceryConstants.PLATFORM_MAC_1015, SauceryConstants.BROWSER_SAFARI, "15", SauceryConstants.SCREENRES_800_600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_IE, "11"),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_IE, "11", SauceryConstants.SCREENRES_800_600),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_EDGE, "99"),
-            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_EDGE, "99", SauceryConstants.SCREENRES_800_600)
-        ];
-
-        public static List<SaucePlatform> EmulatedAndroidPlatforms =>
-        [
-            new AndroidPlatform("Google Pixel 8 Pro GoogleAPI Emulator", "15.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
-        ];
-
-        public static List<SaucePlatform> EmulatedIOSPlatforms =>
-        [
-            new IOSPlatform("iPhone 15 Pro Max Simulator", "17.0", SauceryConstants.DEVICE_ORIENTATION_LANDSCAPE),
-        ];
-
-        public static List<SaucePlatform> RealAndroidDevices =>
-        [
-            new AndroidRealDevice("Google.*", "15"),
-            new AndroidRealDevice("Google Pixel 8 | Android 15 Beta", "15"),
-            new AndroidRealDevice("Google Pixel 8 Pro", "14"),
-            new AndroidRealDevice("Google Pixel 7 Pro", "13"),
-            new AndroidRealDevice("Google Pixel 6a", "12"),
-            new AndroidRealDevice("Google Pixel 4a", "11"),
-            new AndroidRealDevice("Google Pixel 4 XL", "10"),
-            new AndroidRealDevice("Samsung Galaxy Tab S4 10.5", "9")
-        ];
-
-        public static List<SaucePlatform> RealIOSDevices =>
-        [
-            new IOSRealDevice("iPhone 15 Plus", "18"),
-            new IOSRealDevice("iPhone 15 Pro Max", "17"),
-            new IOSRealDevice("iPhone 14 Pro Max", "16"),
-            new IOSRealDevice("iPhone 13 Pro Max", "15"),
-            new IOSRealDevice("iPhone 12 Pro Max", "14"),
-            new IOSRealDevice("iPhone 8 Plus", "13"),
-        ];
     }
 }
---
+++ Saucery.Core.Tests/SauceryTestDataTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Saucery.Core.DataSources;
+using Saucery.Core.Dojo;
+using Saucery.Core.Tests.DataProviders;
+using Shouldly;
+
+namespace Saucery.Core.Tests;
+
+[TestFixture]
+public class SauceryTestDataTests : SauceryTestData {
+    [Test]
+    public void AllPlatformsTest()
+    {
+        SetPlatforms(PlatformDataClass.DesktopPlatforms, PlatformFilter.Emulated);
+
+        Items.ShouldNotBeNull();
+        Items.Count().ShouldBeEquivalentTo(34); //Due to platform expansion.
+    }
+    
+    [Test]
+    public void RealAndroidDeviceTest() {
+        SetPlatforms(PlatformDataClass.RealAndroidDevices, PlatformFilter.RealDevice);
+
+        Items.ShouldNotBeNull();
+        Items.Count().ShouldBeEquivalentTo(PlatformDataClass.RealAndroidDevices.Count);
+    }
+
+    [Test]
+    public void RealAppleDeviceTest() {
+        SetPlatforms(PlatformDataClass.RealIOSDevices, PlatformFilter.RealDevice);
+
+        Items.ShouldNotBeNull();
+        Items.Count().ShouldBeEquivalentTo(PlatformDataClass.RealIOSDevices.Count);
+    }
+
+    [Test]
+    public void EmulatedAndroidDeviceTest() {
+        SetPlatforms(PlatformDataClass.EmulatedAndroidPlatforms, PlatformFilter.Emulated);
+
+        Items.ShouldNotBeNull();
+        Items.Count().ShouldBeEquivalentTo(PlatformDataClass.EmulatedAndroidPlatforms.Count);
+    }
+
+    [Test]
+    public void EmulatedAppleDeviceTest() {
+        SetPlatforms(PlatformDataClass.EmulatedIOSPlatforms, PlatformFilter.Emulated);
+
+        Items.ShouldNotBeNull();
+        Items.Count().ShouldBeEquivalentTo(PlatformDataClass.EmulatedIOSPlatforms.Count);
+    }
+}
