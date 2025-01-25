cbe0fe792610fcdfa71a94d7fce3d2e012e80878
Andrew Gray <andrew.paul.gray@gmail.com>
2025-01-25 18:39:34 +1100

    Add initial Benchmark project

---
+++ Saucery.Benchmark/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using Saucery.Core.Dojo;
+
+namespace Saucery.Benchmark;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private PlatformConfigurator? PlatformConfigurator { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public void All()
+    {
+        PlatformConfigurator = new PlatformConfigurator(PlatformFilter.All);
+    }
+
+    [Benchmark]
+    public void EmulatedOnly()
+    {
+        PlatformConfigurator = new PlatformConfigurator(PlatformFilter.Emulated);
+    }
+
+    [Benchmark]
+    public void RealDeviceOnly()
+    {
+        PlatformConfigurator = new PlatformConfigurator(PlatformFilter.RealDevice);
+    }
+}
---
+++ Saucery.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Saucery.Benchmark;
+
+public class Program {
+    public static void Main(string[] args) {
+        var config = DefaultConfig.Instance;
+        var summary = BenchmarkRunner.Run<Benchmarks>(config, args);
+
+        // Use this to select benchmarks from the console:
+        // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+    }
+}
