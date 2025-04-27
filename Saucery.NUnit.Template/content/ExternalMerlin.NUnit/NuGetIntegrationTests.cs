ae2283db04996f3e1c57698477403ea13e63d12a
Andrew Gray <andrew.paul.gray@gmail.com>
2025-04-27 13:11:44 +1000

    First attempt at template project

---
+++ Saucery.NUnit.Template/content/ExternalMerlin.NUnit/NuGetIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Saucery;
+using Saucery.Core.Dojo;
+using Shouldly;
+using sourceName.PageObjects;
+
+[assembly: LevelOfParallelism(4)]
+
+namespace sourceName;
+
+[TestFixture]
+[Parallelizable]
+[TestFixtureSource(typeof(RequestedPlatformData))]
+public class NuGetIntegrationTests(BrowserVersion browserVersion) : SauceryBase(browserVersion) {
+
+    [Test]
+    [TestCase(5)]
+    [TestCase(4)]
+    public void DataDrivenTest(int data) {
+        var guineaPigPage = new GuineaPigPage(SauceryDriver(), "https://saucelabs.com/");
+
+        guineaPigPage.TypeField(SauceryDriver(), "comments", data.ToString());
+        // verify the page title is correct - this is actually checked as part of the constructor above.
+        Driver?.Title.ShouldContain("I am a page title - Sauce Labs");
+    }
+
+    [Test]
+    public void ClickLinkTest() {
+        var guineaPigPage = new GuineaPigPage(SauceryDriver(), "https://saucelabs.com/");
+        
+        // find and click the link on the page
+        guineaPigPage.ClickLink(SauceryDriver());
+
+        // verify the browser was navigated to the correct page
+        Driver?.Url.ShouldContain("saucelabs.com/test-guinea-pig2.html");
+    }
+}
---
+++ Saucery.NUnit.Template/content/ExternalMerlin.NUnit/RequestedPlatformData.cs
@@ -0,0 +1,31 @@
+﻿using Saucery.Core.DataSources;
+using Saucery.Core.Dojo;
+using Saucery.Core.OnDemand;
+using Saucery.Core.OnDemand.Base;
+using Saucery.Core.Util;
+
+namespace sourceName;
+
+public class RequestedPlatformData : SauceryTestData
+{
+    static RequestedPlatformData()
+    {
+        List<SaucePlatform> platforms =
+        [
+            //Emulated Mobile Platforms
+            new AndroidPlatform("Google Pixel 8 Pro GoogleAPI Emulator", "15.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
+            new IOSPlatform("iPhone 14 Pro Max Simulator", "16.2", SauceryConstants.DEVICE_ORIENTATION_LANDSCAPE),
+
+            //Desktop Platforms
+            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_11, SauceryConstants.BROWSER_CHROME, "123"),
+            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_CHROME, "124", SauceryConstants.SCREENRES_2560_1600)
+        ];
+
+        SetPlatforms(platforms, PlatformFilter.Emulated);
+    }
+}
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 12th July 2014
+* 
+*/
