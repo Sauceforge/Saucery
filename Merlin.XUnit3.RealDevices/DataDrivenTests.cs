d4e905eaa5738da71bbc3ac00288bc66a83205e5
Andrew Gray <andrew.paul.gray@gmail.com>
2025-05-31 15:45:12 +1000

    Add XUnit3 Real Device project and pipeline stages.

---
+++ Merlin.XUnit3.RealDevices/DataDrivenTests.cs
@@ -0,0 +1,24 @@
+﻿using Saucery.Core.Dojo;
+using Saucery.Tests.Common.PageObjects;
+using Saucery.XUnit3;
+
+namespace Merlin.XUnit3.RealDevices;
+
+public class DataDrivenTests(ITestContextAccessor context, BaseFixture baseFixture) : SauceryXBase(context, baseFixture)
+{
+    [Theory]
+    [MemberData(nameof(AllCombinations))]
+    public void DataDrivenTest(BrowserVersion requestedPlatform, int data)
+    {
+        InitialiseDriver(requestedPlatform, _testContextAccessor.Current?.Test!);
+
+        var guineaPigPage = new GuineaPigPage(_baseFixture.SauceryDriver(), "https://saucelabs.com/");
+
+        guineaPigPage.TypeField(_baseFixture.SauceryDriver(), "comments", data.ToString());
+    }
+    
+    public static IEnumerable<object[]> AllCombinations() {
+        _ = RequestedPlatformData.AllPlatforms;
+        return GetAllCombinations([4, 5]);
+    }
+}
