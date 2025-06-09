52b44be8e156c1d3496ba79bc1d1ee6ecde83dc9
Andrew Gray <andrew.paul.gray@gmail.com>
2025-06-09 16:45:04 +1000

    Feat/xunit3 (#278)
    * first attempt at xunit.v3
    
    * .
    
    * Use TestContextAccessor instead.
    
    * add xunit3 stages to pipelines
    
    * Add paths
    
    * Add XUnit3 Real Device project and pipeline stages.
    
    * Add XUnit3 pipeline
    
    * update xunit3 only pipeline
    
    * Add Microsoft.Testing.Extensions.CodeCoverage ref
    
    * oops
    
    * correct env variable
    
    * Add xunit.v3.runner.console and Microsoft.Testing.Platform package.
    
    * update packages
    
    * try this
    
    * add $
    
    * try this 2
    
    * add attributes
    
    * try 3
    
    * update xunit3 real devices project file
    
    * correct label
    
    * try msbuild integration
    
    * tweaks
    
    * try run again
    
    * add json yaml validate
    
    * remove spaces
    
    * try path change
    
    * fix bug
    
    * prepare release

---
+++ Saucery.XUnit3/GlobalSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Saucery.XUnit3;
+
+public static class GlobalSetup
+{
+    [ModuleInitializer]
+    public static void Setup() { } 
+        //XunitContext.EnableExceptionCapture();
+}
+
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 31st May 2025
+* 
+*/
