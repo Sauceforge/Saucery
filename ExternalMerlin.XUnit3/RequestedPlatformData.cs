c80b352b856064ba2b21417afe3d822be7a7548f
Andrew Gray <andrew.paul.gray@gmail.com>
2025-06-28 00:51:59 +1000

    Add ExternalMerlin.XUnit3 project

---
+++ ExternalMerlin.XUnit3/RequestedPlatformData.cs
@@ -0,0 +1,33 @@
+﻿using Saucery.Core.DataSources;
+using Saucery.Core.Dojo;
+using Saucery.Core.OnDemand;
+using Saucery.Core.OnDemand.Base;
+using Saucery.Core.Util;
+
+namespace ExternalMerlin.XUnit3;
+
+public class RequestedPlatformData : SauceryTestData 
+{
+    static RequestedPlatformData() 
+    {
+        List<SaucePlatform> platforms =
+        [
+            //Emulated Mobile Platforms
+            new AndroidPlatform("Google Pixel 8 Pro GoogleAPI Emulator", "15.0", SauceryConstants.DEVICE_ORIENTATION_PORTRAIT),
+            new IOSPlatform("iPhone 14 Pro Max Simulator", "16.2", SauceryConstants.DEVICE_ORIENTATION_LANDSCAPE),
+
+            //Desktop Platforms
+            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_11, SauceryConstants.BROWSER_CHROME, "123"),
+            new DesktopPlatform(SauceryConstants.PLATFORM_WINDOWS_10, SauceryConstants.BROWSER_CHROME, "124", SauceryConstants.SCREENRES_2560_1600)
+        ];
+
+        SetPlatforms(platforms, PlatformFilter.Emulated);
+    }
+
+    public static IEnumerable<object[]> AllPlatforms => GetAllPlatforms();
+}
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 12th July 2014
+* 
+*/
