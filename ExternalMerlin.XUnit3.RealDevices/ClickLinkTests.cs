8c08544a6039cc9c71ea353d4c01f48340f6e79a
Andrew Gray <andrew.paul.gray@gmail.com>
2025-06-28 14:08:15 +1000

    Add ExternalMerlin.XUnit3/RealDevices project

---
+++ ExternalMerlin.XUnit3.RealDevices/ClickLinkTests.cs
@@ -0,0 +1,28 @@
+using ExternalMerlin.XUnit3.RealDevices.PageObjects;
+using Saucery.Core.Dojo;
+using Saucery.XUnit3;
+using Shouldly;
+
+namespace ExternalMerlin.XUnit3.RealDevices;
+
+public class ClickLinkTests(ITestContextAccessor output, BaseFixture baseFixture) : SauceryXBase(output, baseFixture)
+{
+    [Theory]
+    [MemberData(nameof(RequestedPlatformData.AllPlatforms), MemberType = typeof(RequestedPlatformData))]
+    public void ClickLinkTest(BrowserVersion requestedPlatform)
+    {
+        InitialiseDriver(requestedPlatform, _testContextAccessor.Current?.Test!);
+
+        var guineaPigPage = new GuineaPigPage(_baseFixture.SauceryDriver(), "https://saucelabs.com/");
+
+        guineaPigPage.ClickLink(_baseFixture.SauceryDriver());
+
+        // verify the browser was navigated to the correct page
+        _baseFixture.SauceryDriver().Url.ShouldContain("saucelabs.com/test-guinea-pig2.html");
+    }
+}
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 28th June 2025
+* 
+*/
