d0020cab58e2347b075ce07f5ddfadd9f05ec893
Andrew Gray <andrew.paul.gray@gmail.com>
2025-06-29 13:50:59 +1000

    Working Template Test for NUnit

--- Template.Tests/NUnitTemplateTests.cs
+++ Template.Tests/NUnitTemplateTests.cs
@@ -4,18 +4,26 @@ using Shouldly;
 
 namespace Template.Tests;
 
-public class NUnitTemplateTests {
+public class NUnitTemplateTests : IAsyncLifetime {
 
-    private static readonly string TemplateFolder = Path.GetFullPath(
+    private static readonly string _templateFolder = Path.GetFullPath(
             Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Templates", "NUnit"));
 
+    private string? _outputDir;
+    private string? _nUnitTemplateName;
+
+    public Task InitializeAsync() {
+        _outputDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        _nUnitTemplateName = "saucery-nunit";
+        return Task.CompletedTask;
+    }
+
     [Fact]
-    public async Task GeneratesExpectedProject() {
-        var options = new TemplateVerifierOptions("saucery-nunit")
+    public async Task GeneratesExpectedNUnitProject() {
+        var options = new TemplateVerifierOptions(_nUnitTemplateName!)
         {
-            TemplatePath = TemplateFolder,
-            //SnapshotsDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "Snapshots"),
-            OutputDirectory = Path.Combine(Path.GetTempPath(), "NUnit", Path.GetRandomFileName()),
+            TemplatePath = _templateFolder,
+            OutputDirectory = _outputDir,
             TemplateSpecificArgs = ["--name", "MyTestProject"],
             DisableDiffTool = true
         };
@@ -26,10 +34,22 @@ public class NUnitTemplateTests {
         await engine.Execute(options);
 
         // Assert that the project file and sample test file were created
-        var projectFile = Path.Combine(options.OutputDirectory, "MyTestProject.csproj");
-        var testFile = Path.Combine(options.OutputDirectory, "SampleTest.cs");
+        var projectFile = Path.Combine(options.OutputDirectory!, _nUnitTemplateName!, "MyTestProject.csproj");
+        var testFile1 = Path.Combine(options.OutputDirectory!, _nUnitTemplateName!, "NuGetIntegrationTests.cs");
+        var testFile2 = Path.Combine(options.OutputDirectory!, _nUnitTemplateName!, "RequestedPlatformData.cs");
+        var testFile3 = Path.Combine(options.OutputDirectory!, _nUnitTemplateName!, "PageObjects", "GuineaPigPage.cs");
 
         File.Exists(projectFile).ShouldBeTrue("Expected project file to exist");
-        File.Exists(testFile).ShouldBeTrue("Expected sample test to exist");
+        File.Exists(testFile1).ShouldBeTrue("Expected sample test to exist");
+        File.Exists(testFile2).ShouldBeTrue("Expected sample test to exist");
+        File.Exists(testFile3).ShouldBeTrue("Expected sample test to exist");
+    }
+
+    public Task DisposeAsync() {
+        if(Directory.Exists(_outputDir)) {
+            Directory.Delete(_outputDir, recursive: true);
+        }
+
+        return Task.CompletedTask;
     }
 }
