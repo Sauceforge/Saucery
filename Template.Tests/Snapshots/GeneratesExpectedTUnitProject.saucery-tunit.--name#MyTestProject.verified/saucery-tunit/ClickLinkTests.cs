ba36051f7ba89039d92e119e5b6cc672865c6d70
Andrew Gray <andrew.paul.gray@gmail.com>
2025-07-01 17:31:06 +1000

    tunit and xunit3 templates

---
+++ Template.Tests/Snapshots/GeneratesExpectedTUnitProject.saucery-tunit.--name#MyTestProject.verified/saucery-tunit/ClickLinkTests.cs
@@ -0,0 +1,30 @@
+using MyTestProject;
+using MyTestProject.PageObjects;
+using Saucery.Core.Dojo;
+using Saucery.TUnit;
+
+[assembly: ParallelLimiter<MyParallelLimit>]
+
+namespace MyTestProject;
+
+public class ClickLinkTests : SauceryTBase
+{
+    [Test]
+    [MethodDataSource(typeof(RequestedPlatformData), nameof(RequestedPlatformData.AllPlatforms))]
+    public async Task ClickLinkTest(BrowserVersion requestedPlatform)
+    {
+        InitialiseDriver(requestedPlatform);
+
+        var guineaPigPage = new GuineaPigPage(SauceryDriver(), "https://saucelabs.com/");
+
+        guineaPigPage.ClickLink(SauceryDriver());
+
+        // verify the browser was navigated to the correct page
+        await Assert.That(Driver!.Url).Contains("saucelabs.com/test-guinea-pig2.html");
+    }
+}
+/*
+* Copyright Andrew Gray, SauceForge
+* Date: 7th December 2024
+* 
+*/
