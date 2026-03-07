385164f4f070dbbd6310b6b55b0697735436f430
Andrew Gray <118102+agray@users.noreply.github.com>
2026-03-07 13:10:34 +1100

    Add Set 10

---
+++ Saucery.Core.Tests.1920/Set10/ConversionTests.cs
@@ -0,0 +1,19 @@
+﻿using Saucery.Core.Util;
+using Shouldly;
+
+namespace Set10;
+
+public class ConversionTests10
+{
+    [Test]
+    public void SanitisedLongVersionTest()
+    {
+        const string longVersion = "10.0.";
+        var result = longVersion.EndsWith(SauceryConstants.DOT)
+                        ? longVersion.Trim()[..(longVersion.Length - 1)]
+                        : longVersion.Trim();
+        // Optionally log the result if needed
+        // Console.WriteLine($"SanitisedLongVersion returning string '{result}'");
+        result.ShouldBe("10.0");
+    }
+}
---
+++ Saucery.Core.Tests.1920/Set10/MyParallelLimit.cs
@@ -0,0 +1,8 @@
+﻿using TUnit.Core.Interfaces;
+
+namespace Set10;
+
+public record MyParallelLimit10 : IParallelLimit
+{
+    public int Limit => 4;
+}
